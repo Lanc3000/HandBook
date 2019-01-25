b41dd0e0b443682dd0444fac014acbe152aa8045
Lanc3000 <temurt_t@mail.ru>
2019-01-25 23:22:33 +0500

    first commit

---
+++ HandBook/Migrations/20190121161535_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HandBook.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    FatherName = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    Phone = table.Column<int>(type: "varint(20)", nullable: false),
+                    Sex = table.Column<string>(type: "varchar(6)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(250)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ HandBook/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandBook.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName="nvarchar(250)")]
+        [Required]
+        public string FirstName { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        public string LastName { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        public string FatherName { get; set; }
+        [Column(TypeName = "varint(20)")]
+        public int Phone { get; set; }
+        [Column(TypeName = "varchar(6)")]
+        public string Sex { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        public string Country { get; set; }
+    }
+}
